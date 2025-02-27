fd04e159cbd591a2d55393940dc36b2141fe62e5
Andryll Silva <andryll.silva@saipos.com>
2025-02-26 22:12:25 -0300

    170. MY09 - Organizando o Conteúdo com Includes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var conn = require('./../inc/db')
 var express = require('express');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  menus.getMenus().then(results => {
+    res.render('index', {
+      title: 'Restaurante Saboroso!',
+      menus: results
+    })
+  })
 
   conn.query(`
     SELECT * FROM tb_menus ORDER BY title`, (err, results) => {
@@ -31,11 +39,14 @@ router.get("/contacts", function(req, res, next){
 
 router.get("/menus", function(req, res, next){
 
-  res.render('menus', {
-    title: 'Menus - Restaurante Saboroso!',
-    background: 'images/img_bg_1.jpg',
-    h1: 'Saboreie o nosso menu!!'
-  });
+  menus.getMenus().then(results => {
+    res.render('menus', {
+      title: 'Menus - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie o nosso menu!!',
+      menus: results
+    });
+  })
 })
 
 router.get("/reservations", function(req, res, next){
