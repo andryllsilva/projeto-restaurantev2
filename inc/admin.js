d8634f4e8af05545fcbde22864ea2a0b728d0927
Andryll Silva <andryll.silva@saipos.com>
2025-02-27 01:29:16 -0300

    180. MY19 - Formatando Menu Ativo

--- inc/admin.js
+++ inc/admin.js
@@ -1,11 +1,11 @@
 module.exports = {
 
-  getMenus(){
+  getMenus(req){
 
-    return [
+    let menus = [
       {
         text: "Tela Inicial",
-        href: "/admin",
+        href: "/admin/",
         icone: "home",
         active: false
       },
@@ -40,5 +40,12 @@ module.exports = {
         active: false
       }
     ]
+
+    menus.map(menu => {
+
+      if(menu.href === `/admin${req.url}`) menu.active = true;
+    })
+
+    return menus
   }
 }
