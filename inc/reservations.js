834b8a0322e3f5ae6e584198845c894c66216d7e
Andryll Silva <andryll.silva@saipos.com>
2025-02-28 23:31:55 -0300

    191. MY30 - Administração da reserva CREATE

--- inc/reservations.js
+++ inc/reservations.js
@@ -2,7 +2,7 @@ var conn = require("./db")
 
 module.exports = {
 
-  render(req, res, error, success){
+  render(req, res, error, success) {
 
     res.render('reservations', {
       title: 'Reservas - Restaurante Saboroso!',
@@ -14,29 +14,55 @@ module.exports = {
     });
   },
 
-  save(fields){
+  save(fields) {
     return new Promise((resolve, reject) => {
-      let date = fields.date.split('/')
 
-      fields.date = `${date[2]}-${date[1]}-${date[0]}`
+      if (fields.date.indexOf('/') > -1) {
+        let date = fields.date.split('/')
 
-      conn.query(`
+        fields.date = `${date[2]}-${date[1]}-${date[0]}`
+      }
+
+
+
+      let query, params = [
+        fields.name,
+        fields.email,
+        fields.people,
+        fields.date,
+        fields.time
+      ];
+
+      if (parseInt(fields.id) > 0) {
+        query = `
+          UPDATE tb_reservations
+          SET
+              name = ?,
+              email = ?,
+              people = ?,
+              date = ?,
+              time = ?
+            WHERE id = ?
+        `;
+
+        params.push(fields.id)
+
+      } else {
+
+        query = `
         INSERT INTO tb_reservations (name, email, people, date, time)
         VALUES(?, ?, ?, ?, ?)
-        `, [
-          fields.name,
-          fields.email,
-          fields.people,
-          fields.date,
-          fields.time
-        ], (err, results) => {
-  
-          if (err) {
-            reject(err)
-          }else{
-            resolve(results)
-          }
-        })
+        `
+      }
+
+      conn.query(query, params, (err, results) => {
+
+        if (err) {
+          reject(err)
+        } else {
+          resolve(results)
+        }
+      })
     });
   }
 }
